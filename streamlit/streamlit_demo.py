56f7c62e8171a16cc59c15a83bd773da01ca0486
Ryan Massey <114211808+rsmassey@users.noreply.github.com>
2023-03-09 22:41:40 +0900

    Update streamlit_demo.py

--- streamlit/streamlit_demo.py
+++ streamlit/streamlit_demo.py
@@ -86,7 +86,7 @@ def run_prediction(audio_norm, model):
     # Tempo and beats
     tempo, beats = librosa.beat.beat_track(y=audio_norm)
 
-    st.markdown(f"<h2 style='text-align: left; color: red;'> The tempo is {tempo:.1f} beats per minute.</h2>", unsafe_allow_html=True)
+    st.markdown(f"<h2 style='text-align: left;'> The tempo is {tempo:.1f} beats per minute.</h2>", unsafe_allow_html=True)
     # st.markdown(f"<h1 style='text-align: center; color: red;'>The beats are {beats_mean}</h1>", unsafe_allow_html=True)
     
      # Frequencies
